bc525820021c381ce23836a16754d9b7724a97bf
Sunny Bedi <sunnybedi002@yahoo.com>
2021-12-03 08:03:34 -0800

    card.js complete

--- src/components/card.js
+++ src/components/card.js
@@ -1,33 +1,41 @@
-const Card = (article) => {
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
-}
+const cardContainer = document.querySelector("div.cards-container");
 
-const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
-}
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then((response) => {
+    let articleCard = response.data.articles;
+    console.log(articleCard);
+    Object.entries(articleCard).forEach((articleloop) => {
+      console.log(articleloop[1]);
+      articleloop[1].forEach((articles) =>
+        cardContainer.appendChild(articleMaker(articles))
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-export { Card, cardAppender }
+function articleMaker(article) {
+  const card = document.createElement("div");
+  const headLine = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const image = document.createElement("img");
+  const authorName = document.createElement("span");
+
+  card.classList.add("card");
+  headLine.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  card.append(headLine, author);
+  author.append(imgContainer, authorName);
+  imgContainer.appendChild(image);
+
+  headLine.textContent = article.headline;
+  authorName.textContent = article.authorName;
+  image.src = article.authorPhoto;
+
+  return card;
+}
