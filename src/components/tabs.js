e37015028c1f598796e9dd7fb5676ac7732d6539
Sunny Bedi <sunnybedi002@yahoo.com>
2021-12-03 08:06:29 -0800

    tabs.js complete

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,28 +1,22 @@
-const Tabs = (topics) => {
-  // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then((response) => {
+    response.data.topics.forEach((item) => {
+      divTopics.appendChild(tabs(item));
+    });
+
+    console.log(response.data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+function tabs(obj) {
+  const divTab = document.createElement("div");
+  divTab.classList.add("tab");
+  divTab.textContent = obj;
+
+  return divTab;
 }
 
-const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it with a console.log!).
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
-}
-
-export { Tabs, tabsAppender }
+const divTopics = document.querySelector("div.topics");
